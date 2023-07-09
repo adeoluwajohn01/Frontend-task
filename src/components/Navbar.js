e7cf3fcb5d65336185ccb67fe3134412ee3ba1fa
Adedeji Adeoluwa <adeoluwa@Adedejis-MacBook-Pro.local>
2023-07-09 19:09:02 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+
+import React from "react"
+import "../styles/main.css";
+
+function Navbar(){
+
+
+    return (
+        <header>
+            <img src="busha.png"/>
+           <nav>
+             
+             <div className="move">
+             <button className="but"> O </button> 
+             <h3 className="tobi">Oluwatobi Akindunjoye</h3>  
+             </div>
+             
+    
+           </nav>
+           
+
+        </header>
+       
+    );
+}
+
+export default Navbar;
